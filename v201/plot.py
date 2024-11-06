949c82ff68abfcba12d7567cf2ea221a1ad75930
Max Mustermann <max.mustermann@udo.edu>
2024-11-06 22:44:51 +0100

    Änderungen an der Durchführung

--- v201/plot.py
+++ v201/plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
+""" x = np.linspace(0, 10, 1000)
 y = x ** np.sin(x)
 
 fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
@@ -14,9 +14,12 @@ ax2.plot(x, y, label="Kurve")
 ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
 ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
 ax2.legend(loc="best")
-#fig.savefig("build/plot.pdf")
+#fig.savefig("build/plot.pdf") """
 
-#einfachste Lösung
+
+
+
+#Lineare Regression
 plt.rcParams["figure.figsize"] = (6, 4)
 plt.rcParams["font.size"] = 16
 
--- vtraeg/plot.py
+++ vtraeg/plot.py
@@ -1,18 +1,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-ax1.plot(x, y, label="Kurve")
-ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax1.legend(loc="best")
+#Liniare regression
+plt.rcParams["figure.figsize"] = (6, 4)
+plt.rcParams["font.size"] = 16
 
-ax2.plot(x, y, label="Kurve")
-ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax2.legend(loc="best")
+# load data
+x, y = np.genfromtxt("Daten/tqzuaq.txt", unpack=True)
+
+fig, ax = plt.subplots(1, 1, layout="constrained")
+
+ax.plot(x, y, "k.", label="example data")
+ax.set(xlabel=r"$t \,/\, \mathrm{s}$", ylabel=r"$s \,/\, \mathrm{m}$");
+
+params, covariance_matrix = np.polyfit(x, y, deg=1, cov=True)
+
+errors = np.sqrt(np.diag(covariance_matrix))
+
+for name, value, error in zip("ab", params, errors):
+    print(f"{name} = {value:.3f} ± {error:.3f}")
+
+
+
+
+x_plot = np.linspace(0, 600)
+
+fig, ax = plt.subplots(1, 1, layout="constrained")
+
+ax.plot(x, y, "k.", label="Messwerte")
+ax.plot(
+    x_plot,
+    params[0] * x_plot + params[1],
+    label="Lineare Regression",
+    linewidth=1,
+    color="tab:orange",
+)
+
+ax.legend()
+ax.set(xlabel=r"$a²(cm²)$", ylabel=r"$\symbf{T²}(s²)$");
+
+
 
 fig.savefig("build/plot.pdf")
