420b76611a3752f4b0a048f1c151a9ddd1d88a66
Max Mustermann <max.mustermann@udo.edu>
2024-11-09 23:02:57 +0100

    Viele Änderungen an der durchführung und Rechnen geübt bzw gemacht

--- vtraeg/rechner.py
+++ vtraeg/rechner.py
@@ -1,13 +1,32 @@
 from uncertainties import ufloat
 import numpy as np
 import uncertainties.unumpy as unp
+from scipy.stats import sem
+
+#berechnung konstante D
+dataPhi = np.array([22.5 ,45   ,67.5 ,90   ,112.5,135  ,157.5,180  ,202.5,225  ])
+dataF = np.array([0.0000,0.04,0.068,0.09,0.126,0.164,0.172,0.18,0.2,0.28])
+dataD = np.array([0.0000000000,0,0,0,0,0,0,0,0,0])
+#print(dataF)
+i = 0
+while(i<= 9):
+    #print(dataF[i])
+    #print(dataPhi[i])
+    dataD[i] = (dataF[i]*0.292)/(dataPhi[i])
+    #print(dataD[i])
+    i = i+1
+    #print(dataD)
+mwD = np.sum(dataD) / 10
+mwFehlerD = sem(dataD)
+print(mwD , mwFehlerD, "mwD")
+D = ufloat(mwD, mwFehlerD)
 
 #berechnung trägheitsmoment kugel und zylinder
 mk = ufloat(0.6386, 0.001)
 rk = ufloat(0.0605,0.0001)
 
 Tk = (2/5) * mk * rk * rk
-print (Tk ," Trägheit kugel")
+print (Tk ," Trägheit kugel theor")
 
 #zylinder
 mz = ufloat(0.366, 0.001)
@@ -15,7 +34,31 @@ rz = ufloat(0.0493, 0.0001)
 hz = ufloat(0.0995, 0.0001)
 
 Tz = (1/2) * mz * rz * rz
-print(Tz ," Trägheit zylinder")
+print(Tz ," Trägheit zylinder theor")
+
+#Mittelwerte plus fehler Kugel
+dataK = np.array([1.13 ,1.22 ,1.15 ,1.16 ,1.16 ,1.22 ,1.22 ,1.25, 1.16 ,1.19 ])
+mwKugel = np.sum(dataK) / 10
+mwFehlerKugel = sem(dataK)
+print(mwKugel , mwFehlerKugel)
+
+#Mittelwerte plus fehler Zylinder
+dataZ = np.array([0.78,0.65,0.78,0.78,0.78,0.75,0.72,0.79,0.75,0.78 ])
+mwZylinder = np.sum(dataZ) / 10
+mwFehlerZylinder = sem(dataZ)
+print(mwZylinder , mwFehlerZylinder)
+
+#Trägheitsmomente k,z experimentel
+Tk = ufloat(1.1859999999999997, 0.012489995996796807)
+Tz = ufloat(0.756, 0.01359738536958076)
+
+Ik = (Tk*Tk*D)/4*np.pi*np.pi
+print(Ik, "Trägheitsmoment kugel")
+
+Iz = (Tz*Tz*D)/4*np.pi*np.pi
+print(Iz, "Trägheitsmoment zylinder")
+
+
 
 
 
