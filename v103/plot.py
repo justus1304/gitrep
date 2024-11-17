b306f37cc2472f638d83be06c90b21f71a4fa383
Max Mustermann <max.mustermann@udo.edu>
2024-11-17 20:32:43 +0100

    a

--- v103/plot.py
+++ v103/plot.py
@@ -13,6 +13,7 @@ def lrRunder():
     y = y * 0.001
     print (x)
     print (y)
+    
 
     x = (0.6*x**2 + (1/3) * x**3)
 
@@ -21,7 +22,7 @@ def lrRunder():
     params, covariance_matrix = np.polyfit(x, y, deg=1, cov=True)
     errors = np.sqrt(np.diag(covariance_matrix))
     for name, value, error in zip("ab", params, errors):
-        print(f"{name} = {value:.3f} ± {error:.3f}")
+        print(f"{name} = {value:.4f} ± {error:.4f}")
 
     #x-Achse anzeigebereich
     x_plot = np.linspace(0.025, 0.2)
@@ -68,7 +69,7 @@ def lrEckiger():
     params, covariance_matrix = np.polyfit(x, y, deg=1, cov=True)
     errors = np.sqrt(np.diag(covariance_matrix))
     for name, value, error in zip("ab", params, errors):
-        print(f"{name} = {value:.3f} ± {error:.3f}")
+        print(f"{name} = {value:.4f} ± {error:.4f}")
 
     #x-Achse anzeigebereich
     x_plot = np.linspace(0.025, 0.2)
@@ -105,98 +106,144 @@ def beidseitigLinks():
     plt.rcParams["font.size"] = 16
     # daten aus txt laden
     x,a,b = np.genfromtxt("Daten/runderStab.txt", unpack=True, max_rows = 10)
+    x2,c,d = np.genfromtxt("Daten/eckigerStab.txt", unpack=True, max_rows = 10)
     #x aus differenz bilden(Aufgabenspezifisch)
     y = a - b
     y = y * 0.001
     x = x * 0.001
+    #identisch
+    y2 = c - d 
+    y2 = y2 * 0.001
+    x2 = x2 * 0.001
 
-
+    x2 = (3*0.6**2*x - 4*x**3)
     x = (3*0.6**2*x - 4*x**3)
     fig, a = plt.subplots(1, 1, layout="constrained")
 
     params, covariance_matrix = np.polyfit(x, y, deg=1, cov=True)
     errors = np.sqrt(np.diag(covariance_matrix))
     for name, value, error in zip("ab", params, errors):
-        print(f"{name} = {value:.3f} ± {error:.3f}")
+        print(f"{name} = {value:.5f} ± {error:.5f}","Parameter Runder Stab beidseitig links")
 
     #x-Achse anzeigebereich
-    x_plot = np.linspace(0.075, 0.275)
+    x_plot = np.linspace(0.075, 0.225)
     # ??
     fig, ay = plt.subplots(1, 1, layout="constrained")
     #label der Messwerte
-    ay.plot(x, y, "kx", label="Messwerte",markersize=10,)
+    ay.plot(x, y, "kx", label="Messwerte Runder Stab",markersize=10,)
+    ay.plot(x, y2, "k.", label="Meswerte Eckiger Stab",markersize=10,)
     ay.grid(True)
     #ploten der Ausgleichsgeraden
     ay.plot(
         x_plot,
         params[0] * x_plot + params[1],
-        label="Lineare Regression",
+        label="Regression Runder Stab",
         #Dicke der linie
         linewidth=1.5,
         #Farbe
         color="tab:blue",
     )
+    params, covariance_matrix = np.polyfit(x, y2, deg=1, cov=True)
+    errors = np.sqrt(np.diag(covariance_matrix))
+    for name, value, error in zip("ab", params, errors):
+        print(f"{name} = {value:.5f} ± {error:.5f}", "parameter eckiger stab beidseitig links")
+    ay.plot(
+        x_plot,
+        params[0] * x_plot + params[1],
+        label="Regression Eckiger Stab",
+        #Dicke der linie
+        linewidth=1.5,
+        #Farbe
+        color="tab:red",
+    )
     #Legende anzeigen lassen (labels)
-    ay.legend()
+    
+    ay.legend(fontsize = 10)
     #Achsenbeschriftungen
     ay.set(xlabel=r"$3L^2x-4x^3 / \unit{\milli\meter}$", ylabel=r"$\symbf{D} / \unit{\milli\meter}$");
-    fig.savefig("build/beidseitigLinks.pdf")
 
+    ##################################################################################################################################
+    
+    ##############################################################################################################################
+
+    fig.savefig("build/beidseitigLinks.pdf")
 
 
 beidseitigLinks()
 
 
-# def beidseitigRechts():
+
+
+
+def beidseitigRechts():
     
-#     plt.rcParams["figure.figsize"] = (6, 4)
-#     plt.rcParams["font.size"] = 16
-#     # daten aus txt laden
-#     x,a,b = np.genfromtxt("Daten/eckigerStab.txt", unpack=True, max_rows = 10,skip_header=11)
-#     #x aus differenz bilden(Aufgabenspezifisch)
-#     y = a - b
-#     y = y * 0.001
-#     x = x * 0.001
+    plt.rcParams["figure.figsize"] = (6, 4)
+    plt.rcParams["font.size"] = 16
+    # daten aus txt laden
+    x,a,b = np.genfromtxt("Daten/runderStab.txt",skip_header = 11, unpack=True, max_rows = 10)
+    x2,c,d = np.genfromtxt("Daten/eckigerStab.txt",skip_header = 11, unpack=True, max_rows = 10)
+    #x aus differenz bilden(Aufgabenspezifisch)
+    y = a - b
+    y = y * 0.001
+    x = x * 0.001
+    #identisch
+    y2 = c - d 
+    y2 = y2 * 0.001
+    x2 = x2 * 0.001
 
+    
+    x = (4*x**3-12*0.6*x**2+9*0.6**2*x-0.6**3)
+    fig, a = plt.subplots(1, 1, layout="constrained")
 
-#     x = (4*x**3-12*0.6*x**2+9*0.6**2*x-0.6**3)
-#     fig, a = plt.subplots(1, 1, layout="constrained")
+    params, covariance_matrix = np.polyfit(x, y, deg=1, cov=True)
+    errors = np.sqrt(np.diag(covariance_matrix))
+    for name, value, error in zip("ab", params, errors):
+        print(f"{name} = {value:.5f} ± {error:.5f}","Parameter Runder Stab beidseitig rechts")
 
-#     params, covariance_matrix = np.polyfit(x, y, deg=1, cov=True)
-#     errors = np.sqrt(np.diag(covariance_matrix))
-#     for name, value, error in zip("ab", params, errors):
-#         print(f"{name} = {value:.3f} ± {error:.3f}")
+    #x-Achse anzeigebereich
+    x_plot = np.linspace(0.075, 0.225)
+    # ??
+    fig, ay = plt.subplots(1, 1, layout="constrained")
+    #label der Messwerte
+    ay.plot(x, y, "kx", label="Messwerte Runder Stab",markersize=10,)
+    ay.plot(x, y2, "k.", label="Meswerte Eckiger Stab",markersize=10,)
+    ay.grid(True)
+    #ploten der Ausgleichsgeraden
+    ay.plot(
+        x_plot,
+        params[0] * x_plot + params[1],
+        label="Regression Runder Stab",
+        #Dicke der linie
+        linewidth=1.5,
+        #Farbe
+        color="tab:blue",
+    )
+    params, covariance_matrix = np.polyfit(x, y2, deg=1, cov=True)
+    errors = np.sqrt(np.diag(covariance_matrix))
+    for name, value, error in zip("ab", params, errors):
+        print(f"{name} = {value:.5f} ± {error:.5f}", "parameter eckiger stab beidseitig rechts")
+    ay.plot(
+        x_plot,
+        params[0] * x_plot + params[1],
+        label="Regression Eckiger Stab",
+        #Dicke der linie
+        linewidth=1.5,
+        #Farbe
+        color="tab:red",
+    )
+    #Legende anzeigen lassen (labels)
+    
+    ay.legend(fontsize = 10)
+    #Achsenbeschriftungen
+    ay.set(xlabel=r"$(4x^3-12Lx^3+9L^2x-L^3) / \unit{\milli\meter}$", ylabel=r"$\symbf{D} / \unit{\milli\meter}$");
 
-#     #x-Achse anzeigebereich
-#     x_plot = np.linspace(0.075, 0.275)
-#     # ??
-#     fig, ay = plt.subplots(1, 1, layout="constrained")
-#     #label der Messwerte
-#     ay.plot(x, y, "kx", label="Messwerte",markersize=10,)
-#     ay.grid(True)
-#     #ploten der Ausgleichsgeraden
-#     ay.plot(
-#         x_plot,
-#         params[0] * x_plot + params[1],
-#         label="Lineare Regression",
-#         #Dicke der linie
-#         linewidth=1.5,
-#         #Farbe
-#         color="tab:blue",
-#     )
-#     #Legende anzeigen lassen (labels)
-#     ay.legend()
-#     #Achsenbeschriftungen
-#     ay.set(xlabel=r"$3L^2x-4x^3 / \unit{\milli\meter}$", ylabel=r"$\symbf{D} / \unit{\milli\meter}$");
-#     fig.savefig("build/beidseitigLinks.pdf")
+    ##################################################################################################################################
+    
+    ##############################################################################################################################
 
+    fig.savefig("build/rechtsBeidseitig.pdf")
 
-
-# beidseitigRechts()
-
-
-
-
+beidseitigRechts()
 
 
 
--- v103/rechner.py
+++ v103/rechner.py
@@ -3,11 +3,96 @@ import numpy as np
 import uncertainties.unumpy as unp
 from scipy.stats import sem
 
-a = ufloat(0.0000000019 , 0.0000000001)
-G = 6.67430e-11
-mg = 1 #kg
-F = mg*G
-r = 25
-I = (r**4 * np.pi)/2
-E = F / (48*a*I)
-print(E)
+
+#runder Stab einseitig
+def runderStabEinseitig():
+    
+    mg = 0.2 #kg
+    a = ufloat( 0.0100 , 0.0005)
+    
+    G = 9.81
+    F = mg*G
+    r = 0.005
+    I = (r**4 * np.pi)/4
+    E = F / (2*a*I)
+    print(I,"Flt rund")
+    print(repr(E),"E modul rund einseitig")
+runderStabEinseitig()
+
+def eckigerStabEinseitig():
+    
+    mg = 0.2 #kg
+    a = ufloat( 0.0059 , 0.0003)
+    
+    G = 9.81
+    F = mg*G
+    h = 0.01
+    I = (h**4)/12
+    E = F / (2*a*I)
+    print(I,"Flt eckig")
+    print(repr(E),"E modul eckig einseitig")
+eckigerStabEinseitig()
+
+
+print(" ")
+
+
+def runderStabBeidseitigLinks():
+    
+    mg = 1 #kg
+    a = ufloat( 0.00243 , 0.00018)
+    
+    G = 9.81
+    F = mg*G
+    r = 0.005
+    I = (r**4 * np.pi)/4
+    E = F / (48*a*I)
+    
+    print(repr(E),"E modul rund beidseitig links")
+runderStabBeidseitigLinks()
+
+print(" ")
+def eckigerStabBeidseitigLinks():
+    
+    mg = 1 #kg
+    a = ufloat( 0.00189 , 0.00006)
+    
+    G = 9.81
+    F = mg*G
+    h = 0.01
+    I = (h**4)/12
+    E = F / (48*a*I)
+    
+    print(repr(E),"E modul eckig beidseitig links")
+eckigerStabBeidseitigLinks()
+
+
+print(" ")
+################################################################
+def runderStabBeidseitigRechts():
+    
+    mg = 1 #kg
+    a = ufloat( 0.00374 , 0.00014)
+    
+    G = 9.81
+    F = mg*G
+    r = 0.005
+    I = (r**4 * np.pi)/4
+    E = F / (48*a*I)
+    
+    print(repr(E),"E modul rund beidseitig Rechts")
+runderStabBeidseitigRechts()
+print(" ")
+def eckigerStabBeidseitigRechts():
+    
+    mg = 1 #kg
+    a = ufloat( 0.00272 , 0.00008)
+    
+    G = 9.81
+    F = mg*G
+    h = 0.01
+    I = (h**4)/12
+    E = F / (48*a*I)
+    
+    print(repr(E),"E modul eckig beidseitig Rechts")
+eckigerStabBeidseitigRechts()
