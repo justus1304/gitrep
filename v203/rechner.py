03d73dd7488dda7b98e86ce9358a81d8542addab
Max Mustermann <max.mustermann@udo.edu>
2024-11-25 18:26:33 +0100

    a

--- v203/rechner.py
+++ v203/rechner.py
@@ -7,4 +7,16 @@ R = 8.314
 a = ufloat(-3874.9574 , 33.1933)
 
 L = -R*a 
-print(L, "L <1Bar")
+print( "L <1Bar = " , L)
+
+L_a = R * 373
+
+print("L_a = " , L_a)
+
+L_i = L - L_a 
+
+print ("L_i = " , L_i)
+
+L_i_EV = (L_i / (6.022 * 10**(23))) / (1.602 * 10**(-19))
+
+print("L_i in EV = ", L_i_EV)
