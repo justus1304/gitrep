963419589ad14f58b7469741e71f56ffeb535c5e
Guy Lochny <guy.lochny@tu-dortmund.de>
2024-11-29 10:35:11 +0100

    test push

--- v204/plot.py
+++ v204/plot.py
@@ -11,23 +11,7 @@ def verlaufT1T4():
     # Angenommen, die Datei hat Spalten: Zeit, Temperatur T1, Temperatur T4.
     # Überspringen der Kopfzeile (falls vorhanden) mit `skip_header=1`.
 
-<<<<<<< HEAD
-    zeit, temp_t1, temp_t4 = np.genfromtxt('Daten/T1T4.txt', unpack=True)
-
-    # 2. Spalten in Variablen speichern
-    #zeit = daten[:, 0]   # Erste Spalte: Zeit t in s
-    #temp_t1 = daten[:, 1]  # Zweite Spalte: Temperatur T1 in °C
-    #temp_t4 = daten[:, 2]  # Dritte Spalte: Temperatur T4 in °C
-||||||| 908d01f
-    daten = np.genfromtxt('Daten/T1T4.txt', unpack=True)
-
-    # 2. Spalten in Variablen speichern
-    zeit = daten[:, 0]   # Erste Spalte: Zeit t in s
-    temp_t1 = daten[:, 1]  # Zweite Spalte: Temperatur T1 in °C
-    temp_t4 = daten[:, 2]  # Dritte Spalte: Temperatur T4 in °C
-=======
     zeit, temp_T1, temp_T4 = np.genfromtxt('Daten/T1T4.txt', unpack=True)
->>>>>>> refs/remotes/origin/main
 
     # 3. Figure und Achse explizit erstellen
     fig, ax = plt.subplots(figsize=(10, 6))
@@ -36,16 +20,8 @@ def verlaufT1T4():
     ax.plot(zeit, temp_T1, label='Temperatur T1 (°C)', color='red', marker='o')
     ax.plot(zeit, temp_T4, label='Temperatur T4 (°C)', color='blue', marker='x')
 
-<<<<<<< HEAD
-    ax.set_xlim(0, 900)  # x-Achse von 0 bis 1500
-    ax.set_ylim(0, 50)    # y-Achse von 0 bis 45
-||||||| 908d01f
-    ax.set_xlim(0, 1500)  # x-Achse von 0 bis 1500
-    ax.set_ylim(0, 45)    # y-Achse von 0 bis 45
-=======
     ax.set_xlim(0, 1000)  
     ax.set_ylim(0, 55)    
->>>>>>> refs/remotes/origin/main
 
     # 5. Diagramm beschriften
     ax.set_title('Temperaturverlauf über die Zeit')
@@ -86,4 +62,6 @@ def verlaufT5T8():
     fig.savefig("build/tempverlaufT5T8.pdf")
 verlaufT5T8()
 
+
+#test
 
