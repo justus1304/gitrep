fb3cbd2a18cef0cc03e18d0e9a4fb19192be0f45
Guy Lochny <guy.lochny@tu-dortmund.de>
2024-11-30 16:07:49 +0100

    fast fertige statik auswertung

--- v204/plot.py
+++ v204/plot.py
@@ -1,44 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import stats
+
 from uncertainties import ufloat
-from scipy.signal import find_peaks
+
 import uncertainties.unumpy as unp
 from scipy.stats import sem
-
-#berechnung der Konstanten der Stäbe
-
-#Abmessungen in cm
-l_Mb = 9
-b_Mb = 1.2
-h_Mb = 0.4
-
-l_A = 9
-b_A = 1.2
-h_A = 0.4
-
-l_E = 9
-b_E = 1.2
-h_E = 0.4
-
-#Dichte in kg/m^3
-r_M = 8520
-r_A = 2800
-r_E = 8000
-
-#wärmekapazitäten
-c_M = 385
-c_A = 830
-c_E = 400
-
-#abstand thermoelemente xd in (m)
-dx = 0.03
 
 def verlaufT1T4():
     # 1. Daten einlesen mit genfromtxt
     # Angenommen, die Datei hat Spalten: Zeit, Temperatur T1, Temperatur T4.
     # Überspringen der Kopfzeile (falls vorhanden) mit `skip_header=1`.
-
     zeit, temp_T1, temp_T4 = np.genfromtxt('Daten/T1T4.txt', unpack=True)
 
     # 3. Figure und Achse explizit erstellen
@@ -52,7 +23,6 @@ def verlaufT1T4():
     ax.set_ylim(0, 55)    
 
     # 5. Diagramm beschriften
-    ax.set_title('Temperaturverlauf über die Zeit')
     ax.set_xlabel('Zeit (s)')
     ax.set_ylabel('Temperatur (°C)')
     ax.legend()
@@ -80,7 +50,6 @@ def verlaufT5T8():
     ax.set_ylim(0, 55)    
 
     # 5. Diagramm beschriften
-    ax.set_title('Temperaturverlauf über die Zeit')
     ax.set_xlabel('Zeit (s)')
     ax.set_ylabel('Temperatur (°C)')
     ax.legend()
@@ -91,7 +60,7 @@ def verlaufT5T8():
 verlaufT5T8()
 
 
-def plot_temperaturdifferenz():
+def plot_temperaturdifferenz12():
    # Ergebnisse laden
    t, T1, T2 = np.genfromtxt("Daten/T1T2diff.txt", unpack = True)
    Delta_T = T2 - T1
@@ -99,16 +68,30 @@ def plot_temperaturdifferenz():
    plt.figure(figsize=(10, 6))
    plt.plot(t, Delta_T, label='Temperaturdifferenz $\Delta T = T1 - T2$', color='blue')
    plt.xlabel('Zeit [s]')
-   plt.ylabel('Temperaturdifferenz [°C]')
-   plt.title('Temperaturdifferenz über die Zeit')
+   plt.ylabel('T [°C]')
    plt.legend()
    plt.grid(True)
    
    # Plot speichern
-   plt.savefig("plot_differenz.pdf")
-plot_temperaturdifferenz()
+   plt.savefig("plotdiff12.pdf")
+plot_temperaturdifferenz12()
 
-#################################################################################################################################
+def plot_temperaturdifferenz78():
+   # Ergebnisse laden
+   t, T7, T8 = np.genfromtxt("Daten/T7T8diff.txt", unpack = True)
+   Delta_T = T7 - T8
+   # Plot erstellen
+   plt.figure(figsize=(10, 6))
+   plt.plot(t, Delta_T, label='Temperaturdifferenz $\Delta T = T7 - T8$', color='blue')
+   plt.xlabel('Zeit [s]')
+   plt.ylabel('T [°C]')
+   plt.legend()
+   plt.grid(True)
+   # Plot speichern
+   plt.savefig("plotdiff78.pdf")
+plot_temperaturdifferenz78()
+
+
 
 #Dynamische methode Messing
 def messing():
@@ -116,82 +99,21 @@ def messing():
     plt.rcParams["font.size"] = 16
     # daten aus txt laden
     t, T_1, T_2 = np.genfromtxt("Daten/messing.txt", unpack=True)
-    T_1 = T_1 + 273
-    T_2 = T_2 + 273
-    
-    T_1peaks, _ = find_peaks(T_1, height=0)
-    T_1lows, _ = find_peaks(-T_1)
-
-    T_2peaks, _ = find_peaks(T_2, height=0)
-    T_2lows, _ = find_peaks(-T_2)
-    
+    #x aus differenz bilden(Aufgabenspezifisch)
+    #fig, ax = plt.subplots(1, 1, layout="constrained")
     #x-Achse anzeigebereich
     x_plot = np.linspace(0,1000)
     # ??
     fig, ax = plt.subplots(1, 1, layout="constrained")
-    #ploten der Meswerte und Maxima/Minima
-    ax.plot(t, T_1, "kx", label="Temperatur $T_1$",markersize=0.5,)
-    ax.plot(t, T_2, "rx", label="Temperatur $T_2$",markersize=0.5,)
-    ax.plot(t[T_1peaks], T_1[T_1peaks], "rx", label='Maxima')
-    ax.plot(t[T_1lows], T_1[T_1lows], "kx", label='Minima')
-
-    ax.plot(t[T_2peaks], T_2[T_2peaks], "rx")
-    ax.plot(t[T_2lows], T_2[T_2lows], "kx")
+    #label der Messwerte
+    ax.plot(t, T_1, "kx", label="T_1",markersize=1,)
+    ax.plot(t, T_2, "rx", label="T_2",markersize=1,)
     ax.grid(True)
 
-    #phasendifferenz in (s)
-    pd = t[T_1peaks] - t[T_2peaks]
-    print ("T1T2 Delta t (s)= ", pd)
-    Mwpd = ufloat(np.mean(pd), stats.sem(pd))
-    print("Mwpd = ", np.mean(pd), " +- ", stats.sem(pd))
-    
-    #Amplituden in (K)
-#########################berechnung Amplituden A_nah
-    A_nah = [0]*10
-    i = 0
-    while(i<=9):
-        A_nah[i] = 0.5 * (T_1[T_1peaks][i+1] - 0.5 * (T_1[T_1peaks][i+1] - T_1[T_1lows][i]) - T_1[T_1lows][i])
-        i = i + 1
-#########################
-    A_nah = np.array(A_nah)
-    print("A_nah = ", A_nah)
-    
-    
-#########################berechnung Amplituden A_fern
-    A_fern = [0]*10
-    i = 0
-    while(i<=9):
-        A_fern[i] = 0.5 * (T_2[T_2peaks][i+1] - 0.5 * (T_2[T_2peaks][i+1] - T_2[T_2lows][i]) - T_2[T_2lows][i])
-        i = i + 1
-#########################
-    A_fern = np.array(A_fern)
-    print("A_fern = ", A_fern)
-
-    print("")
-    print("MwA_nah = ", np.mean(A_nah), " +- ", stats.sem(A_nah))
-    print("MwA_fern = ", np.mean(A_fern), " +- ", stats.sem(A_fern))
-
-    #ln(anteil)
-
-    print("ln(A_nah / A_fern) ", np.log(A_nah/A_fern))
-
-    Mwln = ufloat(np.mean(np.log(A_nah/A_fern)), stats.sem(np.log(A_nah/A_fern)))
-    print("Mwln(A_nah/A_fern) = ", np.mean(np.log(A_nah/A_fern)), " +- ", stats.sem(np.log(A_nah/A_fern)))
-    
-    #berechnung der wärmeleitfähigkeit k
-    k = (r_M * c_M * (dx)**2)/(2 * Mwpd * Mwln)
-    print(k)
+    #Legende anzeigen lassen (labels)
     ax.legend()
-    #ax.set(xlabel=r"$t/\unit{\second}$ ", ylabel=r"$T/\unit{\kelvin}$");
+    #ax.set_xlim(0.0026, 0.00345)
+    #Achsenbeschriftungen
+    #ax.set(xlabel=r"t/\unit{\sec}$ ", ylabel=r"$T/unit{\degree\celsius}$");
     fig.savefig("build/messingPlot.pdf")
 messing()
-
-
-
-
-
-#Dynamische methode Aluminium
-#def Aluminium():
-#    
-#    fig.savefig("build/aluminiumPlot.pdf")
-#Aluminium()
