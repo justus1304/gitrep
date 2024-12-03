649f2d5aef35b6f92cfd613657529d054ff466b0
Guy Lochny <guy.lochny@tu-dortmund.de>
2024-12-03 15:48:28 +0100

    a

---
+++ v302/rechner.py
@@ -0,0 +1,16 @@
+from uncertainties import ufloat
+import numpy as np
+import uncertainties.unumpy as unp
+from scipy.stats import sem
+
+
+#Wiederstandsmessung
+def wiederstandmessung():
+    arrR2 = []
+    arrR3 = []
+    arrR4 = []
+    Rx = arrR2[i] * (arrR3[i]/arrR4[i])
+wiederstandmessung()  
+
+
+
