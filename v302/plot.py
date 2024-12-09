ed984f2f76bcc90336064b843275e8503df52b5e
Justus Weber <smjuwebe@tu-dortmund.de>
2024-12-09 15:08:28 +0000

    a

--- v302/plot.py
+++ v302/plot.py
@@ -9,26 +9,26 @@ plt.rcParams["font.size"] = 16
 
 w0 = 1 / ((1000*663)*10**(-9))
 v0 = w0 / (2*np.pi)
-
+print(v0)
 #UbrUs = (1/9)*(w**2*R**2*C**2-1)**2 / ((1-w**2*R**2*C**2)**2+9*w**2*R**2*C**2)
 
 x, y = np.genfromtxt("daten/wienrob.txt", unpack=True)
 
 fig, ax = plt.subplots(1, 1, layout="constrained")
 
-
+print(x)
 
 
 x_plot = np.linspace(0, 0.6)
 
 fig, ax = plt.subplots(1, 1, layout="constrained")
 ax.set_yscale("log")
-ax.plot((x*2*np.pi)/w0, np.sqrt((1/9)*((((x*2*np.pi)/w0)**2-1)**2 / ((1-((x*2*np.pi)/w0)**2)**2+9*((x*2*np.pi)/w0)**2))))
-ax.plot((x*2*np.pi)/w0, 0.001*y, "k.", label="Messwerte")
+ax.plot(x/v0, np.sqrt((1/9)*((((x*2*np.pi)/w0)**2-1)**2 / ((1-((x*2*np.pi)/w0)**2)**2+9*((x*2*np.pi)/w0)**2))))
+ax.plot(x/v0, 0.001*y, "k.", label="Messwerte")
 
 
 ax.legend()
-ax.set(xlabel=r"$\unit{\omega}$", ylabel=r"$\frac{U_{Br}}{U_{s}}$");
+ax.set(xlabel=r"$\Omega$", ylabel=r"$\frac{U_{Br}}{U_{s}}$");
 
 
 
