b33b94866ead22766af104bf26b4346cba8989c0
Max Mustermann <max.mustermann@udo.edu>
2024-12-14 19:55:01 +0100

    a

--- v308/plot.py
+++ v308/plot.py
@@ -1,18 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import uncertainties.unumpy as unp
+from scipy.stats import sem
+mu0 = 4*np.pi*(10)**(-7)
+##Kleine Spule 
+def kleineSpule():
+    x,y = np.genfromtxt("Daten/mvsklein.txt" ,unpack=True)
+    
+    fig, ax = plt.subplots(1, 1, layout="constrained")
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+    x_plot = np.linspace(0, 0.6)
+    
+    ax.plot(x, y, "k.", label="Messwerte")
+    ax.legend()
+    #ax.set(xlabel=r"$\Omega$", ylabel=r"$\frac{U_{Br}}{U_{s}}$");
+    fig.savefig("build/plot.pdf")
 
-fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-ax1.plot(x, y, label="Kurve")
-ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax1.legend(loc="best")
+    ##mittelwert berechnen
+    a,b = np.genfromtxt("Daten/mvsklein.txt" ,unpack=True, skip_header = 6, skip_footer = 5)
+    #Mittelwert
+    My = np.mean(b)
+    #fehler Mittelwert
+    FMy = sem(b)
+    print("Magnetfeldstärke kleine Spule = (", My ,", ", FMy, ")")
+    I = 1 #A
+    n = 100
+    l = 0.08
 
-ax2.plot(x, y, label="Kurve")
-ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax2.legend(loc="best")
+    print("Theoretischer Wert Magnetfeldstärke kleine Spule = ", (mu0*n*I)/(l))
 
-fig.savefig("build/plot.pdf")
+kleineSpule()
