3355c20d63909dfc51fa93b8b57991ebb11eeabc
Max Mustermann <max.mustermann@udo.edu>
2025-01-15 20:21:30 +0100

    s

--- v351/plot.py
+++ v351/plot.py
@@ -7,7 +7,7 @@ import numpy as np
 import scipy.optimize
 import uncertainties as unc
 import uncertainties.unumpy as unp
-
+from scipy.optimize import curve_fit
 
 #includs curve fit
 
@@ -29,14 +29,39 @@ def f(x, a, b):
 
 
 
+def sigmoid(x, a, b, c):
+    return a / (1 + np.exp(-(x - b))) + c
 
 
+#def rechteck():
+#    # Solution
+#    u, z ,x = np.genfromtxt("Daten/viereck.txt", unpack=True)
+#    y = 10**(z/20)*0,775
+#    
+#    params, covariance_matrix = curve_fit(f, x, y)
+#    
+#    for name, value in zip("ab", params):
+#        print(f"{name} = {value:8.3f} ")
+#    
+#    fig = plt.figure(layout="constrained")
+#    ax = fig.add_subplot()
+#    ax.plot(x, y, "k.",label = "Messwerte")
+#    
+#    ax.plot(xa, f(xa, *unp.nominal_values(params)), label="Fit")
+#    
+#    ax.legend()
+#    ax.set(xlabel=r"$n$", ylabel=r"$\unit{\volt}$");
+#    
+#    fig.savefig("build/viereck.pdf")
+#    
+#rechteck()  
+#
 
 
 def rechteck():
     # Solution
     u, z ,x = np.genfromtxt("Daten/viereck.txt", unpack=True)
-    y = 10**(z/20)
+    y = 10**(z/20)*0.775
     print("Rechteck =", y)
     xa = np.linspace(0,15)
     
@@ -124,3 +149,32 @@ def saege():
     #     … = scipy.optimize.curve_fit(…, **kwargs)
     #     … """ """
 saege()  
+
+
+
+
+
+#def rechteckCurveFit():
+#    params, covariance_matrix = curve_fit(sigmoid, x, y, p0=(-1, 40, 1))
+#
+#
+#    uncertainties = np.sqrt(np.diag(covariance_matrix))
+#
+#    for name, value, uncertainty in zip("abc", params, uncertainties):
+#        print(f"{name} = {value:8.3f} ± {uncertainty:.3f}")
+#    ax.cla()
+#    x_plot = np.linspace(0, 50, 1000)
+#
+#    ax.plot(x, y, "o", label="Messwerte")
+#    ax.plot(x_plot, sigmoid(x_plot, *params), "-", label="Sigmoid Fit")
+#
+#    ax.legend()
+#    ax.set(xlabel=r"Temperatur / °C", ylabel=r"$GP$")
+#    fig
+#
+#    
+#rechteckCurveFit()  
+
+
+
+
