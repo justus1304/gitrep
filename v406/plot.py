5a266ac00a32c9fcbd7868abf221facd54615045
Justus Weber <smjuwebe@tu-dortmund.de>
2025-04-09 13:45:04 +0100

    s

--- v406/plot.py
+++ v406/plot.py
@@ -19,18 +19,24 @@ def ucurve_fit(f, x, y, **kwargs):
     return unc.correlated_values(popt, pcov)
 
 #Zu Fitende Funktion
-def g(A, x, b):
+def g(phi1, A, b):
     #return A**2 * (np.sinc(b * x))**2 * (np.cos(c * x))**2
-    return A**2 * (np.sinc(b * x))**2
+    return A**2 * (np.sinc(b * phi1))**2
+
+def f(x, A, b, c):
+    return A**2 * (np.sinc(b * x))**2 * (np.cos(c * x))**2
+
 
 def teil1():
     # Solution
     x, I = np.genfromtxt("Daten/ESfix.txt", unpack=True)
-    #x = x*10**(-3)
-    #I = I*10**(-6)
-    x = x/1.26
-    xa = np.linspace(-0.02,0.02)
-    
+    x = x*10**(-3)
+    I = I*10**(-6)
+    phi1 = x/1.26
+    xa = np.linspace(-0.025,0.025, 2000)
+    #phi1 = np.full_like(x,0)
+    #for i in range(len(x)):
+    #    phi1[i] = np.arctan((x[i])/1.26)
     #Ucurve fit ausführen
     params, covariance_matrix = curve_fit(g, x, I,p0 = (0.00015,1000))
     #err = np.sqrt(np.diag(cov))
@@ -40,7 +46,7 @@ def teil1():
 
     fig = plt.figure(layout="constrained")
     ax = fig.add_subplot()
-    ax.plot(x, I, "k.",label = "Messwerte")
+    ax.plot(phi1, I, "k.",label = "Messwerte")
     
     ax.plot(xa, g(xa, *params), label="Fit")
     
@@ -54,3 +60,37 @@ def teil1():
     fig.savefig("build/teil1.pdf")
     
 teil1()  
+
+
+def teil2():
+    # Solution
+    x, I = np.genfromtxt("Daten/DS.txt", unpack=True)
+    x = x*10**(-3)
+    I = I*10**(-6)
+    phi2 = x/1.26
+    xa = np.linspace(-0.025,0.025)
+    #phi1 = np.full_like(x,0)
+    #for i in range(len(x)):
+    #    phi1[i] = np.arctan((x[i])/1.26)
+    #Ucurve fit ausführen
+    params, covariance_matrix = curve_fit(f, phi2, I,p0 = (5.5*10**(-4),1*10**(3),10000))
+    #err = np.sqrt(np.diag(cov))
+    print("a*((x**b))")
+    for char, p in zip("Abc", params):
+        print(f"{char} = {p}")
+
+    fig = plt.figure(layout="constrained")
+    ax = fig.add_subplot()
+    ax.plot(phi2, I, "k.",label = "Messwerte")
+    
+    ax.plot(xa, f(xa, *params), label="Fit")
+    
+    ax.legend()
+    #Achsenbeschriftungen für den Plot
+    ax.set(xlabel=r"$x (\unit{\milli\meter})$", ylabel=r"$I(\unit{\micro\ampere})$");
+    ax.grid(True)
+    #x achsen abstände (wahlweise)
+    #ax.set_xticks([0, np.pi, 2 * np.pi], [0, "$\pi$", "$2\pi$"])
+
+    fig.savefig("build/teil2.pdf")
+teil2()  
