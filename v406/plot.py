049041e1d8b424bf9824856c14d6fa74ef2bb3a1
Max Mustermann <max.mustermann@udo.edu>
2025-04-10 20:42:29 +0200

    s

--- v406/plot.py
+++ v406/plot.py
@@ -22,9 +22,11 @@ def ucurve_fit(f, x, y, **kwargs):
 def g(phi1, A, b):
     #return A**2 * (np.sinc(b * x))**2 * (np.cos(c * x))**2
     return A**2 * (np.sinc(b * phi1))**2
+    #return A**2 * b**2 * ((600*10**(-9))/(np.pi * b * phi1))**2 * (np.sin((np.pi * b * phi1) / (600*10**(-9))))**2
 
 def f(x, A, b, c):
     return A**2 * (np.sinc(b * x))**2 * (np.cos(c * x))**2
+    #return 4 * (np.cos( (np.pi * 1.26 * np.sin(phi) ) /633*10**(-9) ) )**2 * (633*10**(-9) / (np.pi * b * np.sin(phi)))**2 * (np.sin((np.pi * b * np.sin(phi)) / 633*10**(-9)))**2
 
 
 def teil1():
@@ -39,6 +41,7 @@ def teil1():
     #    phi1[i] = np.arctan((x[i])/1.26)
     #Ucurve fit ausführen
     params, covariance_matrix = curve_fit(g, phi1, I,p0 = (0.00015,1000))
+    #params, covariance_matrix = curve_fit(g, phi1, I,p0 = (26,77))
     #err = np.sqrt(np.diag(cov))
     print("a*((x**b))")
     for char, p in zip("Ab", params):
@@ -64,16 +67,20 @@ teil1()
 
 def teil2():
     # Solution
-    x, I = np.genfromtxt("Daten/DS.txt", unpack=True)
+    x, I = np.genfromtxt("Daten/DSNeu.txt", unpack=True)
     x = x*10**(-3)
     I = I*10**(-6)
-    phi2 = x/1.26
-    xa = np.linspace(-0.025,0.025)
+    #phi2 = x/1.26
+    phi2 = np.full_like(x,0)
+    for i in range(len(x)):
+        phi2[i] = np.arctan((x[i]) / 1.26)
+    xa = np.linspace(-0.025,0.025,200000)
     #phi1 = np.full_like(x,0)
     #for i in range(len(x)):
     #    phi1[i] = np.arctan((x[i])/1.26)
     #Ucurve fit ausführen
-    params, covariance_matrix = curve_fit(f, phi2, I,p0 = (5.5*10**(-4),1*10**(3),10000))
+    params, covariance_matrix = curve_fit(f, x, I,p0 = (5.5*10**(-4),1*10**(3),10000))
+    #params, covariance_matrix = curve_fit(f, phi2, I,p0 = (5 * 10**(-7),1, 1))
     #err = np.sqrt(np.diag(cov))
     print("a*((x**b))")
     for char, p in zip("Abc", params):
@@ -81,7 +88,8 @@ def teil2():
 
     fig = plt.figure(layout="constrained")
     ax = fig.add_subplot()
-    ax.plot(phi2, I, "k.",label = "Messwerte")
+    ax.bar(x, I, width=0.00001, color='red')
+    ax.plot(x, I, "kx",label = "Messwerte", markersize=2)
     
     ax.plot(xa, f(xa, *params), label="Fit")
     
