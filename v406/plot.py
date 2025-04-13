ff1ddf5686bc0587e8a8cfd50a7ad11ec0b3ac4b
Max Mustermann <max.mustermann@udo.edu>
2025-04-13 14:25:14 +0200

    s

--- v406/plot.py
+++ v406/plot.py
@@ -34,8 +34,8 @@ def teil1():
     x, I = np.genfromtxt("Daten/ESfix.txt", unpack=True)
     x = x*10**(0)
     I = I*10**(0)
-    phi1 = x/1.26
-    xa = np.linspace(-25,25, 2000)
+    phi1 = x/1260
+    xa = np.linspace(-0.025,0.025, 2000)
     #phi1 = np.full_like(x,0)
     #for i in range(len(x)):
     #    phi1[i] = np.arctan((x[i])/1.26)
@@ -66,20 +66,22 @@ teil1()
 
 
 def teil2():
+    
+
     # Solution
     x, I = np.genfromtxt("Daten/DSNeu.txt", unpack=True)
-    x = x*10**(0) #- 25
+    x = np.arctan(x/1260) #- 25
     I = I*10**(0)
     #phi2 = x/1.26
     phi2 = np.full_like(x,0)
     for i in range(len(x)):
         phi2[i] = np.arctan((x[i]) / 1.26)
-    xa = np.linspace(-25,25,200000)
+    xa = np.linspace(-0.025,0.025,200000)
     #phi1 = np.full_like(x,0)
     #for i in range(len(x)):
     #    phi1[i] = np.arctan((x[i])/1.26)
     #Ucurve fit ausführen
-    params, covariance_matrix = curve_fit(f, x, I,p0 = (60,1,1, -0.1))
+    params, covariance_matrix = curve_fit(f, x, I,p0 = (8,10,0.1, -0.0001))
     #params, covariance_matrix = curve_fit(f, phi2, I,p0 = (5 * 10**(-7),1, 1))
     #err = np.sqrt(np.diag(cov))
     print("a*((x**b))")
@@ -102,3 +104,64 @@ def teil2():
 
     fig.savefig("build/teil2.pdf")
 teil2()  
+
+
+def teil3():
+    # Solution
+    x, I = np.genfromtxt("Daten/ESfix.txt", unpack=True)
+    x = x*10**(0)
+    I = I*10**(0)
+    phi1 = x/1260
+    xa = np.linspace(-0.025,0.025, 2000)
+    #phi1 = np.full_like(x,0)
+    #for i in range(len(x)):
+    #    phi1[i] = np.arctan((x[i])/1.26)
+    #Ucurve fit ausführen
+    params, covariance_matrix = curve_fit(g, phi1, I,p0 = (0.15,0.1))
+    #params, covariance_matrix = curve_fit(g, phi1, I,p0 = (26,77))
+    #err = np.sqrt(np.diag(cov))
+    print("a*((x**b))")
+    for char, p in zip("Ab", params):
+        print(f"{char} = {p}")
+
+    fig = plt.figure(layout="constrained")
+    ax = fig.add_subplot()
+    ax.plot(phi1, I, "k.",label = "Messwerte")
+    
+    ax.plot(xa, g(xa, *params), label="Fit")
+
+    # Solution
+    x, I = np.genfromtxt("Daten/DSNeu.txt", unpack=True)
+    x = np.arctan(x/1260) #- 25
+    I = I*10**(0)
+    #phi2 = x/1.26
+    phi2 = np.full_like(x,0)
+    for i in range(len(x)):
+        phi2[i] = np.arctan((x[i]) / 1.26)
+    xa = np.linspace(-0.025,0.025,200000)
+    #phi1 = np.full_like(x,0)
+    #for i in range(len(x)):
+    #    phi1[i] = np.arctan((x[i])/1.26)
+    #Ucurve fit ausführen
+    params, covariance_matrix = curve_fit(f, x, I,p0 = (8,10,0.1, -0.0001))
+    #params, covariance_matrix = curve_fit(f, phi2, I,p0 = (5 * 10**(-7),1, 1))
+    #err = np.sqrt(np.diag(cov))
+    print("a*((x**b))")
+    for char, p in zip("Absd", params):
+        print(f"{char} = {p}")
+
+    
+    ax.bar(x, I, width=0.00001, color='red')
+    ax.plot(x, I, "kx",label = "Messwerte", markersize=2)
+    
+    ax.plot(xa, f(xa, *params), label="Fit")
+    
+    ax.legend()
+    #Achsenbeschriftungen für den Plot
+    ax.set(xlabel=r"$x (\unit{\milli\meter})$", ylabel=r"$I(\unit{\micro\ampere})$");
+    ax.grid(True)
+    #x achsen abstände (wahlweise)
+    #ax.set_xticks([0, np.pi, 2 * np.pi], [0, "$\pi$", "$2\pi$"])
+
+    fig.savefig("build/teil3.pdf")
+teil3()  
