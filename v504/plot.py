e60730a07cf2a2b6fae0b61eb118ca706e39841b
Guy Lochny <guy.lochny@tu-dortmund.de>
2025-04-15 22:32:00 +0200

    bisschen auswertung

---
+++ v504/plot.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Figure erstellen
+fig, ax = plt.subplots(figsize=(10, 6))  # Größe anpassbar
+
+# Daten laden und plotten (jede Datei einzeln)
+# KL_1
+x1, y1 = np.loadtxt("Daten/KL_1.txt", unpack=True)
+ax.plot(x1, y1, "x", markersize=5, label=r"$I_\mathrm{H} = 1{,}8\,$ A")
+
+# KL_2
+x2, y2 = np.loadtxt("Daten/KL_2.txt", unpack=True)
+ax.plot(x2, y2, "x", markersize=5, label=r"$I_\mathrm{H} = 1{,}8\,$ A")
+
+# KL_3
+x3, y3 = np.loadtxt("Daten/KL_3.txt", unpack=True)
+ax.plot(x3, y3, "x", markersize=5, label=r"$I_\mathrm{H} = 1{,}8\,$ A")
+
+# KL_4
+x4, y4 = np.loadtxt("Daten/KL_4.txt", unpack=True)
+ax.plot(x4, y4, "x", markersize=5, label=r"$I_\mathrm{H} = 5{,}0\,$ A")
+
+# KL_5
+x5, y5 = np.loadtxt("Daten/KL_5.txt", unpack=True)
+ax.plot(x5, y5, "x", markersize=5, label=r"$I_\mathrm{H} = 5{,}0\,$ A")
+
+# Achsenbeschriftungen und Legende
+ax.set_xlabel("Spannung U [V]")
+ax.set_ylabel("Strom I [mA]")
+ax.legend(framealpha=1)  # Legende mit Transparenz
+ax.grid(True, linestyle="--", alpha=0.5)  # Gitterlinien
+
+# Speichern
+fig.savefig("build/mw_plot.pdf")
