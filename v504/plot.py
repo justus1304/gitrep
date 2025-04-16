db8aa083d2973f25e11910df893e77707a329794
Guy Lochny <guy.lochny@tu-dortmund.de>
2025-04-16 11:40:39 +0200

    save

--- v504/plot.py
+++ v504/plot.py
@@ -11,24 +11,25 @@ ax.plot(x1, y1, "x", markersize=5, label=r"$I_\mathrm{H} = 1{,}8\,$ A")
 
 # KL_2
 x2, y2 = np.loadtxt("Daten/KL_2.txt", unpack=True)
-ax.plot(x2, y2, "x", markersize=5, label=r"$I_\mathrm{H} = 1{,}8\,$ A")
+ax.plot(x2, y2, "x", markersize=5, label=r"$I_\mathrm{H} = 2{,}0\,$ A")
 
 # KL_3
 x3, y3 = np.loadtxt("Daten/KL_3.txt", unpack=True)
-ax.plot(x3, y3, "x", markersize=5, label=r"$I_\mathrm{H} = 1{,}8\,$ A")
+ax.plot(x3, y3, "x", markersize=5, label=r"$I_\mathrm{H} = 2{,}1\,$ A")
 
 # KL_4
 x4, y4 = np.loadtxt("Daten/KL_4.txt", unpack=True)
-ax.plot(x4, y4, "x", markersize=5, label=r"$I_\mathrm{H} = 5{,}0\,$ A")
+ax.plot(x4, y4, "x", markersize=5, label=r"$I_\mathrm{H} = 2{,}3\,$ A")
 
 # KL_5
 x5, y5 = np.loadtxt("Daten/KL_5.txt", unpack=True)
-ax.plot(x5, y5, "x", markersize=5, label=r"$I_\mathrm{H} = 5{,}0\,$ A")
+ax.plot(x5, y5, "x", markersize=5, label=r"$I_\mathrm{H} = 2{,}5\,$ A")
 
 # Achsenbeschriftungen und Legende
-ax.set_xlabel("Spannung U [V]")
-ax.set_ylabel("Strom I [mA]")
-ax.legend(framealpha=1)  # Legende mit Transparenz
+ax.set_xlabel("Spannung U [V]", fontsize=20)
+ax.set_ylabel("Strom I [mA]", fontsize=20)
+ax.legend(framealpha=1, fontsize=20)  # Legende mit Transparenz
+ax.tick_params(axis='both', which='major', labelsize=17) # Skalierungszahlen
 ax.grid(True, linestyle="--", alpha=0.5)  # Gitterlinien
 
 # Speichern
