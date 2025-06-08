8eddc9b41dc735e462d29b993d74af8924fab341
Max Mustermann <max.mustermann@udo.edu>
2025-06-08 16:31:13 +0200

    s

--- v701/plot.py
+++ v701/plot.py
@@ -1,18 +1,215 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.optimize
+import uncertainties as unc
+import uncertainties.unumpy as unp
+from scipy.constants import c,h, m_e, e
+from uncertainties import ufloat
+from scipy.stats import poisson, norm
+#Energie berechnen
+def energie(channel,druck):
+    E_max = 4
+    channel_max = 1159
+    channel_dif = channel / channel_max 
+    E = E_max * channel_dif
+    return E
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+def eff_length(x_0, p):
+    p_0 = 1.01
+    x = x_0 * (p / p_0)
+    return x
 
-fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-ax1.plot(x, y, label="Kurve")
-ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax1.legend(loc="best")
+def f(m,x,b):
+    return m * x + b
 
-ax2.plot(x, y, label="Kurve")
-ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax2.legend(loc="best")
+def mittelwert(a):
+    x = 0
+    for i in range(len(a-1)):
+        x = x + a[i]
+    
+    return x / len(a)
+def standartabweichung(a):
+    mw = mittelwert(a)
+    a = a-mw 
+    a = a**2
+    x = 0
+    for i in range(len(a-1)):
+        x = x + a[i]
+    return np.sqrt(x / len(a))
 
-fig.savefig("build/plot.pdf")
+
+def abstand_4():
+    print("Abstand: 4cm")
+    x = np.linspace(0.00,0.036,10000)
+    x_0 = 0.04 #m
+#Daten Einlesen
+    p,n,c = np.genfromtxt("daten/x0ist4.txt",unpack = True)
+#Einheiten umrechnen mbar in bar 
+    p = p*10**(-3)
+
+    fig, ax =plt.subplots()
+#plotten der Messwerte
+    ax.plot(eff_length(x_0,p),energie(c,p),"k.",label = 'Energien')
+#liniare ausgleichhsrechnung
+    params,cov = np.polyfit(eff_length(x_0,p),energie(c,p), 1,cov=True) 
+    m, b = params 
+    m_err, b_err = np.sqrt(np.diag(cov))
+#wert und Fehler ausgeben 
+    print(f"Steigung: {m:.7f} ± {m_err:.7f}")
+    print(f"Achsenabschnitt: {b:.7f} ± {b_err:.7f}")
+    print(-ufloat(b,b_err)/ufloat(m,m_err))
+#Ausgleichsrechnung ploten 
+    ax.plot(x,m * x + b,label = 'regression')
+    
+    #ax.set_xlabel(r"Effektive Weglänge $ /\unit{\meter}$")
+    #ax.set_ylabel(r"Energie$/\unit{\mega\electronvolt}$")
+    ax.legend()
+    ax.grid(True)
+#Senkrechte linie falls benoetigt
+    #ax.axvline(x = 560,color = 'red', linestyle='--')
+    fig.savefig("build/abstand4.pdf")
+#Ausfuehren der funktion
+abstand_4()
+
+
+def abstand_7():
+    print("Abstand: 7cm")
+    x = np.linspace(0.00,0.036,10000)
+    x_0 = 0.07 #m
+
+    p,n,c = np.genfromtxt("daten/x0ist7.txt",unpack = True)
+    p = p*10**(-3)
+
+    fig, ax =plt.subplots()
+
+    ax.plot(eff_length(x_0,p),energie(c,p),"k.",label = 'Energien')
+
+    params,cov = np.polyfit(eff_length(x_0,p),energie(c,p), 1,cov=True) 
+    m, b = params 
+    m_err, b_err = np.sqrt(np.diag(cov))
+
+    print(f"Steigung: {m:.7f} ± {m_err:.7f}")
+    print(f"Achsenabschnitt: {b:.7f} ± {b_err:.7f}")
+    print(-ufloat(b,b_err)/ufloat(m,m_err))
+
+    ax.plot(x,m * x + b,label = 'regrssion')
+    
+    #ax.set_xlabel(r"Effektive Weglänge $ /\unit{\meter}$")
+    #ax.set_ylabel(r"Energie$/\unit{\mega\electronvolt}$")
+    ax.legend()
+    ax.grid(True)
+#Senkrechte linie falls benoetigt
+    #ax.axvline(x = 560,color = 'red', linestyle='--')
+    fig.savefig("build/abstand7.pdf")
+#Ausfuehren der funktion
+abstand_7()
+
+
+
+def anzahl_4():
+    print("Anzahl, Abstand: 4cm ")
+    x = np.linspace(0.00,0.064,10000)
+    x_0 = 0.07 #m
+
+    p,n,c = np.genfromtxt("daten/x0ist4.txt",unpack = True)
+    p = p*10**(-3)
+
+    fig, ax =plt.subplots()
+
+    ax.plot(eff_length(x_0,p),n,"k.",label = 'Pulszahl')
+
+    params,cov = np.polyfit(eff_length(x_0,p)[14:],n[14:], 1,cov=True) 
+    m, b = params 
+    m_err, b_err = np.sqrt(np.diag(cov))
+
+    print(f"Steigung: {m:.7f} ± {m_err:.7f}")
+    print(f"Achsenabschnitt: {b:.7f} ± {b_err:.7f}")
+    print(-ufloat(b,b_err)/ufloat(m,m_err))
+#ploten der ausgleichsgerade 
+    ax.plot(x,m * x + b, label = 'regression')
+#ploten auf halber maximaler hoehe
+    ax.plot(x,f(0,x,np.max(n)/2),label = 'Halbe maximale pulszahl')
+    
+    sp = (np.max(n)/2-ufloat(b,b_err)) / ufloat(m,m_err)
+    print("schnittpunkt : " ,sp,np.max(n)/2)
+#ploten des Schnittpunktes
+    ax.plot(sp.nominal_value,f(m,sp.nominal_value,b),'rx',label = 'Schnittpunkt')
+    #ax.set_xlabel(r"Effektive Weglänge $ /\unit{\meter}$")
+    #ax.set_ylabel(r"Impulse pro 200s")
+    ax.legend()
+    ax.grid(True)
+#Senkrechte linie falls benoetigt
+    #ax.axvline(x = 560,color = 'red', linestyle='--')
+    fig.savefig("build/anzahl4.pdf")
+#Ausfuehren der funktion
+anzahl_4()
+
+def anzahl_7():
+    print("Anzahl, Abstand: 7cm ")
+    x = np.linspace(0.00,0.036,10000)
+    x_0 = 0.07 #m
+
+    p,n,c = np.genfromtxt("daten/x0ist7.txt",unpack = True)
+    p = p*10**(-3)
+
+    fig, ax =plt.subplots()
+
+    ax.plot(eff_length(x_0,p),n,"k.",label = 'Pulszahl')
+
+    params,cov = np.polyfit(eff_length(x_0,p)[6:],n[6:], 1,cov=True) 
+    m, b = params 
+    m_err, b_err = np.sqrt(np.diag(cov))
+
+    print(f"Steigung: {m:.7f} ± {m_err:.7f}")
+    print(f"Achsenabschnitt: {b:.7f} ± {b_err:.7f}")
+    print(-ufloat(b,b_err)/ufloat(m,m_err))
+#ploten der ausgleichsgerade 
+    ax.plot(x,m * x + b, label = 'regression')
+#ploten auf halber maximaler hoehe
+    ax.plot(x,f(0,x,np.max(n)/2),label = 'Halbe maximale pulszahl')
+    sp = (np.max(n)/2-ufloat(b,b_err)) / ufloat(m,m_err) 
+    print("schnittpunkt : " ,sp,np.max(n)/2)
+#ploten des Schnittpunktes
+    ax.plot(sp.nominal_value,f(m,sp.nominal_value,b),'rx',label = 'Schnittpunkt')
+    #ax.set_xlabel(r"Effektive Weglänge $ /\unit{\meter}$")
+    #ax.set_ylabel(r"Impulse pro 200s")
+    ax.legend()
+    ax.grid(True)
+#Senkrechte linie falls benoetigt
+    #ax.axvline(x = 560,color = 'red', linestyle='--')
+    fig.savefig("build/anzahl7.pdf")
+#Ausfuehren der funktion
+anzahl_7()
+
+def histogramm_1():
+    # Beispiel-Daten erzeugen
+
+    daten = np.genfromtxt("daten/100mw.txt")
+    daten2 = daten + np.sqrt(daten)
+    daten3 = daten - np.sqrt(daten)
+    fig, ax =plt.subplots()
+    # Histogramm zeichnen
+    #ax.hist(daten, bins=10, facecolor='none', edgecolor='black', linewidth=3)
+    #ax.hist(daten2, bins=10, facecolor='none', edgecolor='red', linewidth=2, linestyle = '--')
+    #ax.hist(daten3, bins=10, facecolor='none', edgecolor='yellow', linewidth=2, linestyle = '--')
+    x = np.linspace(1700,2000,1000)
+    y = norm.pdf(x,mittelwert(daten),standartabweichung(daten))
+    ax.plot(x,y,label = 'Gauß')
+    #gauß = 1/(standartabweichung(daten) * np.sqrt(2*np.pi)) * np.exp(-(daten-mittelwert(daten))**2/(2*standartabweichung(daten)**2))
+    #ax.hist(gauß, bins=10, facecolor='none', edgecolor='blue', linewidth=2, linestyle = '--')
+    
+    #ax.hist(daten2, bins=10, edgecolor='blue')
+    print("mittelwert = ", ufloat(mittelwert(daten),standartabweichung(daten)/np.sqrt(len(daten))))
+    print("varianz = ", standartabweichung(daten))
+    # Achsenbeschriftungen und Titel
+    ax.set_xlabel('Werte')
+    ax.set_ylabel('Häufigkeit')
+   
+    # Anzeigen
+    ax.legend()
+    ax.grid(True)
+#Senkrechte linie falls benoetigt
+    #ax.axvline(x = 560,color = 'red', linestyle='--')
+    fig.savefig("build/histogram_1.pdf")
+histogramm_1()
+
