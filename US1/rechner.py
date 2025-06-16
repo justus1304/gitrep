47b3810155246439575243039131eee73678963f
Max Mustermann <max.mustermann@udo.edu>
2025-06-16 15:02:39 +0200

    s

--- US1/rechner.py
+++ US1/rechner.py
@@ -4,4 +4,6 @@ import uncertainties.unumpy as unp
 from scipy.stats import sem
 
 a = [0.5,0.6,0.7,0.5,0.7,0.7]
+b = ufloat(np.mean(a), sem(a))
 print("Mittewert = ", np.mean(a), sem(a))
+print(ufloat(2744,14)*b)
